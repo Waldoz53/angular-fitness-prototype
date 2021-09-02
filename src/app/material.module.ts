34c0f79345c63e6fbb0b160821c38496c571df39
Waldoz53 <35583388+Waldoz53@users.noreply.github.com>
2021-09-01 18:05:43 -0600

    creating sign up form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button"; //how to import Material in Material 9+
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule],
-    exports: [MatButtonModule, MatIconModule]
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
+    ]
 })
 export class MaterialModule {}
