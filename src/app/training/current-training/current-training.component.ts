522f69fa9839fa363eb8a59c9fcacdc713b5b0e1
Waldoz53 <35583388+Waldoz53@users.noreply.github.com>
2021-09-04 16:59:01 -0600

    added current and new training systems/timers

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
+  progress = 0;
+  timer!: number;
+  message = "You can do it!"
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.startOrResumeTimer();
+  }
+  
+  startOrResumeTimer() {
+    //uses window.setInterval otherwise editors throw an error that doesn't mean anything? need to research this more.
+    this.timer = window.setInterval(() => {
+      this.progress += 2; //as close to making progress each second (1.666...) without making it annoying to read
+      if (this.progress >= 75) {
+        this.message = "You're almost there!"
+      }
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+        this.message = "You did it!"
+      }
+    }, 1000);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
   }
 
 }
